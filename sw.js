91830101e170088915087fd84eb1d5e99b6a836d
timri86 <richard.ngu.tic@gmail.com>
2021-03-26 18:46:30 +0100

    offline manage

--- sw.js
+++ sw.js
@@ -4,3 +4,11 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate',evt => {
     console.log(evt);
 })
+
+self.addEventListener('fetch', evt=>{
+    if(!navigator.onLine){
+        evt.respondWith(new  Response('pas de connexion'));
+    }
+
+
+})
